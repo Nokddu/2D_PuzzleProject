9b1269710079939ff287f94c7ec0561e2ff24fcf
Kim Jun Hyeong <kheami35@gmail.com>
2025-09-02 10:03:29 +0900

    [ Init ] 필요한 이미지 추가

---
+++ 2D Puzzle Project/Assets/Scripts/UI/HealthUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    [SerializeField] private Image[] healthImg;
+
+    
+    private void UpdateUI()
+    {
+        for(int i = 0; i < healthImg.Length; i++)
+        {
+            
+        }
+    }
+}
