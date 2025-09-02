2877f50a5e5c4e131802dbe8a61bb0d7092f23fb
Kim Jun Hyeong <kheami35@gmail.com>
2025-09-02 10:31:09 +0900

    [ feat ] Foothold 발판

---
+++ 2D Puzzle Project/Assets/Scripts/Interact/Foothold.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foothold : MonoBehaviour
+{
+    [SerializeField] private Sprite onSprite;
+    [SerializeField] private Sprite offSprite;
+    [SerializeField] private Foothold[] otherFootholds;
+
+    private bool isOn = false;
+    private SpriteRenderer SpriteRenderer;
+
+    private void Awake()
+    {
+        SpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        SpriteRenderer.sprite = offSprite;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isOn = !isOn;
+
+        SpriteRenderer.sprite = isOn ? onSprite : offSprite;
+
+        if(isOn && otherFootholds.Length > 0)
+        {
+            foreach(var foothold in otherFootholds)
+            {
+                foothold.FootholdOff();
+            }
+        }
+    }
+
+    private void FootholdOff()
+    {
+        isOn = false;
+        SpriteRenderer.sprite = offSprite;
+    }
+}
