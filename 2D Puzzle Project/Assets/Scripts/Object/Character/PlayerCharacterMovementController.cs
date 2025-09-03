363c6dc8abbc604e69bd9ae59bcb69fbc3e01817
Glory Day <alwaysgloryday@gmail.com>
2025-09-03 19:51:07 +0900

    Fix: Player character jump system

--- 2D Puzzle Project/Assets/Scripts/Object/Character/PlayerCharacterMovementController.cs
+++ 2D Puzzle Project/Assets/Scripts/Object/Character/PlayerCharacterMovementController.cs
@@ -169,7 +169,7 @@ namespace Backend.Object.Character
 
         private void Jump(InputAction.CallbackContext context)
         {
-            if (isJumpable == false)
+            if (isJumpable == false || isMoving)
             {
                 return;
             }
