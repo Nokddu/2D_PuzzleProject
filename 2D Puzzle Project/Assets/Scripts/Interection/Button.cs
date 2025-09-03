9cd6169a64e1a78d5f323f5509525f60a0593054
Nokddu <sksksksk103@naver.com>
2025-09-03 13:04:20 +0900

    [Feat] ChapterScene02 임시 완료

--- 2D Puzzle Project/Assets/Scripts/Interection/Button.cs
+++ 2D Puzzle Project/Assets/Scripts/Interection/Button.cs
@@ -6,10 +6,14 @@ public class Button : MonoBehaviour, IInteractable
 {
     [SerializeField] private float ButtonCheckRadius = 3f;
     [SerializeField] private ChairManager chair;
-
-    private void Update()
+    [SerializeField] private GameObject wall;
+    [SerializeField] private GameObject failedImg;
+    private void Awake()
     {
-        
+        if (GameManager.Ins.GameCleared[0] == true)
+        {
+            wall.SetActive(false);
+        }
     }
 
     public void Interact()
@@ -18,14 +22,26 @@ public class Button : MonoBehaviour, IInteractable
         {
             Debug.Log("정답");
             // 정답이다 연금술사
-
+            GameManager.Ins.PuzzleClear(0);
+            wall.SetActive(false);
         }
         else
         {
             Debug.Log("오답");
 
+            StartCoroutine("showImage");
+
             chair.ResetChairs();
         }
+    }
+
+    IEnumerator showImage()
+    {
+        wall.SetActive(false);
+        failedImg.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        failedImg.SetActive(false);
+        wall.SetActive(true);
     }
 
     public void TryCheck()
