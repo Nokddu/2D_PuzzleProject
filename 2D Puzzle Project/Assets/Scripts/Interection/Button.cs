84f36b05a1a5944b208c765f26d0ba7785231c12
Nokddu <sksksksk103@naver.com>
2025-09-04 17:24:10 +0900

    [Fix] 왼쪽방 버튼 수정

--- 2D Puzzle Project/Assets/Scripts/Interection/Button.cs
+++ 2D Puzzle Project/Assets/Scripts/Interection/Button.cs
@@ -2,27 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour, IInteractable
+public class Button : MonoBehaviour
 {
     [SerializeField] private float ButtonCheckRadius = 3f;
     [SerializeField] protected ChairManager chair;
+    [SerializeField] private GameObject wall;
+    [SerializeField] private GameObject failedImg;
 
-
-
-    public virtual void Interact()
+    private void Awake()
     {
-        
+        if (GameManager.Ins.GameCleared[0] == true)
+        {
+            wall.SetActive(false);
+        }
     }
 
-    
-
-    public void TryCheck()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, ButtonCheckRadius);
-
-        foreach (var col in collider)
+        if (chair.CheckChairs())
         {
-            
+            Debug.Log("정답");
+            // 정답이다 연금술사
+            GameManager.Ins.PuzzleClear(0);
+            wall.SetActive(false);
         }
+        else
+        {
+            Debug.Log("오답");
+
+            StartCoroutine("showImage");
+        }
+    }
+
+    IEnumerator showImage()
+    {
+        wall.SetActive(false);
+        failedImg.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        GameManager.Ins.ResetGame();
     }
 }
